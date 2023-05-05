3a525e4df8d4a2841239d3ddea7921aa5fe2b200
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-05-05 19:13:07 +0300

    [MD-4]: Personal Dictionary Theme.

---
+++ AppModules/PersonalDictionary/Source/Theme.swift
@@ -0,0 +1,46 @@
+//
+//  Theme.swift
+//  PersonalDictionary
+//
+//  Created by Maksim Ivanov on 06.05.2023.
+//
+
+import SwiftUI
+
+struct Theme {
+
+    let backgroundColor: Color
+
+    let goldColor: Color
+
+    let cellColor: Color
+
+    let textColor: Color
+
+    let secondaryTextColor: Color
+
+    let normalFont: Font
+
+    private init(backgroundColor: Color,
+                 goldColor: Color,
+                 cellColor: Color,
+                 textColor: Color,
+                 secondaryTextColor: Color,
+                 normalFont: Font) {
+        self.backgroundColor = backgroundColor
+        self.goldColor = goldColor
+        self.cellColor = cellColor
+        self.textColor = textColor
+        self.secondaryTextColor = secondaryTextColor
+        self.normalFont = normalFont
+    }
+
+    static let data = Theme(
+        backgroundColor: Color("Background", bundle: Bundle.module),
+        goldColor: Color(red: 1.00, green: 0.84, blue: 0.00),
+        cellColor: Color("Cell", bundle: Bundle.module),
+        textColor: Color("Text", bundle: Bundle.module),
+        secondaryTextColor: Color("SecondaryText", bundle: Bundle.module),
+        normalFont: Font.system(size: 17)
+    )
+}
