b6e5c4b99318ff3936a29e32bb796ca00758c741
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-05-05 17:38:52 +0300

    [MD-3]: Lang, LangType.

---
+++ AppModules/PersonalDictionary/Source/DataClass/LangType.swift
@@ -0,0 +1,12 @@
+//
+//  LangType.swift
+//  PersonalDictionary
+//
+//  Created by Maxim Ivanov on 09.11.2021.
+//
+
+/// A type of a selected language
+enum LangType {
+    case source /// a source language
+    case target /// a target language
+}
