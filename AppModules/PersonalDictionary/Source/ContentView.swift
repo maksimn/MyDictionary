8aeb96a9d98e17f9e033c2a2d6055a4769059180
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-05-07 02:12:48 +0300

    [MD-6]: LangPicker feature.

--- AppModules/PersonalDictionary/Source/ContentView.swift
+++ AppModules/PersonalDictionary/Source/ContentView.swift
@@ -5,29 +5,40 @@
 //  Created by Maksim Ivanov on 05.05.2023.
 //
 
+import ComposableArchitecture
 import SwiftUI
+
+let configFactory = ConfigFactory()
+let config = configFactory.config()
+
+let emptyLang = Lang(id: .init(raw: 0), name: "", shortName: "")
+let lang = config.allLangs.first ?? emptyLang
+
+let store = Store(
+    initialState: LangPicker.State(lang: lang, langType: .source),
+    reducer: LangPicker()._printChanges()
+)
 
 public struct ContentView: View {
 
     private let theme = Theme.data
 
-    public init() {
-        let factory = ConfigFactory()
-        let config = factory.config()
-
-        print(config)
-    }
+    public init() { }
 
     public var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("My dictionary")
-                .foregroundColor(theme.textColor)
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack {
+                if viewStore.isHidden {
+                    Button("Show LangPicker") {
+                        viewStore.send(.show(lang: config.allLangs[2], langType: .source))
+                    }
+                } else {
+                    LangPickerView(allLangs: config.allLangs, store: store)
+                }
+            }
+            .background(theme.backgroundColor)
+            .padding()
         }
-        .background(theme.backgroundColor)
-        .padding()
     }
 }
 
---
+++ AppModules/PersonalDictionary/Source/UIFeature/LangPicker/View/LangPickerView.swift
@@ -0,0 +1,53 @@
+//
+//  LangPickerView.swift
+//  PersonalDictionary
+//
+//  Created by Maksim Ivanov on 07.05.2023.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct LangPickerView: View {
+
+    let allLangs: [Lang]
+    let store: StoreOf<LangPicker>
+
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Picker(
+                    "Languages",
+                    selection: viewStore.binding(
+                        get: { $0.lang.name },
+                        send: { name in
+                            guard let lang = allLangs.first(where: { $0.name == name }) else { return .none }
+
+                            return .langChanged(lang)
+                        }
+                    )
+                ) {
+                    ForEach(allLangs.map { LangDO(name: $0.name) }) { lang in
+                        Text(lang.name)
+                    }
+                }
+                Button(action: {
+                    viewStore.send(.hide)
+                }) {
+                    Text("Select")
+                        .padding(.init(top: 2, leading: 20, bottom: 2, trailing: 20))
+                        .foregroundColor(.white)
+                        .background(Color(red: 0.34, green: 0.34, blue: 0.34))
+                }
+                .cornerRadius(8)
+                .padding(.init(top: 0, leading: 0, bottom: 16, trailing: 0))
+            }
+            .pickerStyle(.wheel)
+        }
+    }
+}
+
+private struct LangDO: Identifiable {
+    var name: String
+    var id: String { self.name }
+}
