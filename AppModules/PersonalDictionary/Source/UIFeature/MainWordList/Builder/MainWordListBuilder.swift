dcc248ff05d35efe9e15dc5e9120193d4e2c9262
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-05-09 17:21:22 +0300

    [TD-11]: List for MainWordListView.

--- AppModules/PersonalDictionary/Source/UIFeature/MainWordList/Builder/MainWordListBuilder.swift
+++ AppModules/PersonalDictionary/Source/UIFeature/MainWordList/Builder/MainWordListBuilder.swift
@@ -15,9 +15,21 @@ struct MainWordListBuilder {
     private let store: StoreOf<MainWordList>
 
     init(config: Config) {
+        let lang1 = config.langData.allLangs[0]
+        let lang2 = config.langData.allLangs[1]
+        let words = [
+            "Banana", "Potato", "Tomato",
+            "Banana", "Potato", "Tomato",
+            "Banana", "Potato", "Tomato",
+            "Banana", "Potato", "Tomato"
+        ]
         self.config = config
         self.store = Store(
-            initialState: .init(),
+            initialState: .init(
+                wordList: IdentifiedArrayOf(uniqueElements: words.map {
+                    IdentifiedWord(word: Word(text: $0, sourceLang: lang1, targetLang: lang2))
+                })
+            ),
             reducer: MainWordList(
                 langRepository: LangRepositoryImpl(userDefaults: UserDefaults.standard, data: config.langData)
             )._printChanges()
