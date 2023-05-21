2ffc4026a60ddc662a9698a34cf2fc223d7884b3
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-05-21 22:49:21 +0300

    [MD-25]: PersonalDictionary update to use latest CoreModule version.

--- AppModules/PersonalDictionary/Source/DictionaryService/PonsDictionaryEntryDecoder.swift
+++ AppModules/PersonalDictionary/Source/DictionaryService/PonsDictionaryEntryDecoder.swift
@@ -64,17 +64,3 @@ struct PonsResponseDataHitsRomsArab: Codable {
 struct PonsResponseDataHitsRomsArabsTranslation: Codable {
     let target: String
 }
-
-extension Array where Element: Hashable {
-    func removingDuplicates() -> [Element] {
-        var addedDict = [Element: Bool]()
-
-        return filter {
-            addedDict.updateValue(true, forKey: $0) == nil
-        }
-    }
-
-    mutating func removeDuplicates() {
-        self = self.removingDuplicates()
-    }
-}
